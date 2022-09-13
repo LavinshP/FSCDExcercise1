9396a094e1544666c47633dfa573f3b766d7490c
LavinshP <113482193+LavinshP@users.noreply.github.com>
2022-09-13 19:47:30 -0400

    First Commit Ex 1
    Input two variables and then performed operations on it.

---
+++ Excercise1.py
@@ -0,0 +1,14 @@
+val1 = int(input("Enter the first value: "))
+val2 = int(input("Enter the second value: "))
+
+add = val1+val2
+sub = val1-val2
+mul = val1*val2
+div = val1/val2
+pwr = val1**val2
+
+print('Addition: '+ str(add))
+print('Subraction: '+ str(sub))
+print('Multiplication: '+ str(mul))
+print('Division: '+ str(div))
+print('Power: '+ str(pwr))
